5a15e88f1e5e4640fca4d2ed0e026fde54155b5e
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-26 12:00:03 +0100

    vat calculator

---
+++ README.md
@@ -0,0 +1,13 @@
+# Typescript boilerpate projekt
+
+Klona ner projektet
+
+## Installation
+
+Installera alla paket med `npm i`
+
+## Utveckling
+
+Starta dev miljön med kommandot `npm start` för att köra typescript kompilatorn i watch-läget.
+
+**Kom också ihåg att starta Live Server.**
---
+++ src/code.ts
@@ -0,0 +1,80 @@
+window.addEventListener('load', main);
+
+//Global variabel
+let selectedMoms = 1.25;
+
+function main() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    const button25 = document.getElementById('moms-25')
+    const button12 = document.getElementById('moms-12')
+    const button6 = document.getElementById('moms-6')
+
+    button25?.addEventListener('click', applyMoms25);
+    button12?.addEventListener('click', applyMoms12);
+    button6?.addEventListener('click', applyMoms6);
+
+    const incMomsInput = document.getElementById('inc-moms');
+    incMomsInput?.addEventListener('input', updateBasedOnIncMoms);
+    
+    const exMomsInput = document.getElementById('ex-moms');
+    exMomsInput?.addEventListener('input', updateBasedOnExMoms);
+}
+
+function updateBasedOnIncMoms() {
+    const incMomsInput = document.getElementById('inc-moms') as HTMLInputElement;
+    const exMomsInput = document.getElementById('ex-moms') as HTMLInputElement;
+    const sumMomsInput = document.getElementById('sum-moms') as HTMLInputElement;
+    
+
+    const incMoms = Number(incMomsInput.value);
+    const exMoms = incMoms / selectedMoms;
+    const momsSum = incMoms - exMoms;
+
+
+    exMomsInput.value = String(exMoms);
+    sumMomsInput.value = String(momsSum)
+}
+function updateBasedOnExMoms() {
+    const incMomsInput = document.getElementById('inc-moms') as HTMLInputElement;
+    const exMomsInput = document.getElementById('ex-moms') as HTMLInputElement;
+    const sumMomsInput = document.getElementById('sum-moms') as HTMLInputElement;
+    
+
+    const exMoms = Number(exMomsInput.value);
+    const incMoms = exMoms / selectedMoms;
+    const momsSum = incMoms - exMoms;
+
+
+    incMomsInput.value = String(incMoms);
+    sumMomsInput.value = String(momsSum)
+}
+
+function applyMoms25(event: MouseEvent) {
+    selectedMoms = 1.25;
+    updateButtonsUI(event);
+    updateBasedOnIncMoms();
+
+}
+function applyMoms12(event: MouseEvent) {
+    selectedMoms = 1.12;
+    updateButtonsUI(event);
+    updateBasedOnIncMoms();
+
+}
+function applyMoms6(event: MouseEvent) {
+    selectedMoms = 1.06;
+    updateButtonsUI(event);
+    updateBasedOnIncMoms();
+}
+
+function updateButtonsUI(event: MouseEvent) {
+    const activebutton = document.querySelector('.active');
+    activebutton?.classList.remove('active');
+
+    const button = event.target as HTMLButtonElement;
+    button.classList.add('active');
+
+}
